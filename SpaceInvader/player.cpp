b3116a5c31364b23de3ba709db804623365f8557
Sneyder <64097040+sneyder1711@users.noreply.github.com>
2023-07-16 12:08:51 -0500

    player.cpp
    se creo la estructura de los metodos necesarios

--- SpaceInvader/player.cpp
+++ SpaceInvader/player.cpp
@@ -1,6 +1,33 @@
 #include "player.h"
+#include "game.h"
 
-player::player()
+Player::Player(Game *game)
+    : game(game)
 {
+    // Set up player's initial position, speed, and size
+}
 
+QRect Player::getRect()
+{
+    return rect;
+}
+
+void Player::moveLeft()
+{
+    // Move the player to the left
+}
+
+void Player::moveRight()
+{
+    // Move the player to the right
+}
+
+void Player::stopMoving()
+{
+    // Stop player's movement
+}
+
+void Player::shoot()
+{
+    // Create a bullet and add it to the playerBullets list in the game
 }
