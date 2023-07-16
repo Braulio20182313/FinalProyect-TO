d91de2dbfbf41ed88f81a0d669076744232150cb
Sneyder <64097040+sneyder1711@users.noreply.github.com>
2023-07-16 12:14:53 -0500

    bullet.h
    se crea los atributos y metodos necesarios para los disparos

--- SpaceInvader/bullet.h
+++ SpaceInvader/bullet.h
@@ -1,11 +1,22 @@
 #ifndef BULLET_H
 #define BULLET_H
 
+#include <QRect>
 
-class bullet
+class Game;
+
+class Bullet
 {
 public:
-    bullet();
+    Bullet(Game *game, bool isPlayerBullet, int x, int y);
+    QRect getRect();
+    void move();
+
+private:
+    QRect rect;
+    int speed;
+    bool playerBullet;
+    Game *game;
 };
 
 #endif // BULLET_H
