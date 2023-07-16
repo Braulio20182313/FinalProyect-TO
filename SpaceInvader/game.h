df090ba2cfb99c3e2473e94ced8e2e2aa451d6aa
Sneyder <64097040+sneyder1711@users.noreply.github.com>
2023-07-16 12:02:37 -0500

    game.h
    se crearon los atributos necesarios asi como algunas funciones base

--- SpaceInvader/game.h
+++ SpaceInvader/game.h
@@ -1,11 +1,40 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <QWidget>
+#include <QTimer>
+#include <QKeyEvent>
 
-class game
+class Player;
+class Enemy;
+
+class Game : public QWidget
 {
+    Q_OBJECT
+
 public:
-    game();
+    Game(QWidget *parent = nullptr);
+    ~Game();
+
+protected:
+    void keyPressEvent(QKeyEvent *event);
+    void keyReleaseEvent(QKeyEvent *event);
+    void paintEvent(QPaintEvent *event);
+    void updateGame();
+
+private:
+    void initializeGame();
+    void checkCollisions();
+    void gameOver();
+
+    Player *player;
+    QList<Enemy*> enemies;
+    QList<Bullet*> playerBullets;
+    QList<Bullet*> enemyBullets;
+    bool gameRunning;
+    bool leftPressed;
+    bool rightPressed;
+    QTimer *gameTimer;
 };
 
 #endif // GAME_H
