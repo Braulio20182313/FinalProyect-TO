5b89477ecb32cedc975045cf50cfd089b7a24668
Sneyder <64097040+sneyder1711@users.noreply.github.com>
2023-07-16 12:12:43 -0500

    enemy.cpp
    al igual que con player se crea la estructura para los metodos necesarios

--- SpaceInvader/enemy.cpp
+++ SpaceInvader/enemy.cpp
@@ -1,6 +1,23 @@
 #include "enemy.h"
+#include "game.h"
 
-enemy::enemy()
+Enemy::Enemy(Game *game)
+    : game(game)
 {
+    // Set up enemy's initial position, speed, and size
+}
 
+QRect Enemy::getRect()
+{
+    return rect;
+}
+
+void Enemy::move()
+{
+    // Move the enemy in a specific pattern (e.g., left-right movement)
+}
+
+void Enemy::shoot()
+{
+    // Create a bullet and add it to the enemyBullets list in the game
 }
