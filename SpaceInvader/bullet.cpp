bf2d66f1337e62ebd84d3838f0def5d3a9a01764
Sneyder <64097040+sneyder1711@users.noreply.github.com>
2023-07-16 12:16:46 -0500

    bullet.cpp
    se crea la estructura base de los metodos para los disparos

--- SpaceInvader/bullet.cpp
+++ SpaceInvader/bullet.cpp
@@ -1,6 +1,18 @@
 #include "bullet.h"
+#include "game.h"
 
-bullet::bullet()
+Bullet::Bullet(Game *game, bool isPlayerBullet, int x, int y)
+    : game(game), playerBullet(isPlayerBullet)
 {
+    // Set up bullet's initial position, speed, and size based on playerBullet
+}
 
+QRect Bullet::getRect()
+{
+    return rect;
+}
+
+void Bullet::move()
+{
+    // Move the bullet upwards if it's a player bullet or downwards if it's an enemy bullet
 }
